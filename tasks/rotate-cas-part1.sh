b67ffdd46bf425f5fed948e617db64b0c1316d7c
Dan Jahner <djahner@pivotal.io>
2017-10-14 21:10:39 +0200

    Fix scripts

--- tasks/rotate-cas-part1.sh
+++ tasks/rotate-cas-part1.sh
@@ -13,6 +13,6 @@ tar xvf credhub-linux-1.5.0-beta.46.tgz
 echo $CAS_TO_ROTATE | jq -r .[] | while read object; do
   ./credhub regenerate --name $object > /dev/null
   #Hackily set the value to include the prior certificate for gradual rotation
-  ./credhub set --type certificate --name $object --certificate "$(credhub g -n $object --versions 2 --output-json | jq -r .versions[0].value.certificate & credhub g -n $object --versions 2 --output-json | jq -r .versions[1].value.certificate)" --private "$(credhub g -n $object --output-json | jq -r .value.private_key)"
+  ./credhub set --type certificate --name $object --certificate "$(./credhub g -n $object --versions 2 --output-json | jq -r .versions[0].value.certificate & ./credhub g -n $object --versions 2 --output-json | jq -r .versions[1].value.certificate)" --private "$(./credhub g -n $object --output-json | jq -r .value.private_key)"
 done
 
--- tasks/rotate-cas-part3.sh
+++ tasks/rotate-cas-part3.sh
@@ -12,6 +12,6 @@ tar xvf credhub-linux-1.5.0-beta.46.tgz
 
 echo $CAS_TO_ROTATE | jq -r .[] | while read object; do
   #Hackily unset the value to remove the prior certificate
-  ./credhub set --type certificate --name $object --certificate "$(credhub g -n $object --versions 2 --output-json | jq -r .versions[1].value.certificate)" --private "$(credhub g -n $object --versions 2 --output-json | jq -r .versions[1].value.private_key)" 
+  ./credhub set --type certificate --name $object --certificate "$(./credhub g -n $object --versions 2 --output-json | jq -r .versions[1].value.certificate)" --private "$(./credhub g -n $object --versions 2 --output-json | jq -r .versions[1].value.private_key)" 
 done
 
