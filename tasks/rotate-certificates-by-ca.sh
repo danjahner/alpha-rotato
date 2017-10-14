1eb54a133e19f24e389865f461cdea420273bf7f
Dan Jahner <djahner@pivotal.io>
2017-10-14 15:04:06 +0200

    Fix header, skip tls validation for now

--- tasks/rotate-certificates-by-ca.sh
+++ tasks/rotate-certificates-by-ca.sh
@@ -9,8 +9,9 @@ TOKEN="$(credhub --token)"
 echo $SIGNED_BY_TO_ROTATE | jq -r .[] | while read object; do
   curl https://${CREDHUB_SERVER}/api/v1/bulk-regenerate \
   -X POST \
-  -H "authorization: bearer ${TOKEN}" \
+  -H "authorization: ${TOKEN}" \
   -H 'content-type: application/json' \
-  -d "{\"signed_by\": \"${object}\""
+  -d "{\"signed_by\": \"${object}\"" \ 
+  -k
 done
 
