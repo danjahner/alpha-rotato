cfb6a9434a8e932ac287aba51d5f52a7e7ab79a5
Dan Jahner <djahner@pivotal.io>
2017-10-18 15:07:39 -0700

    Use and echo IDs to be more explicit about what is changing

--- tasks/rotate-cas-part1.sh
+++ tasks/rotate-cas-part1.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-set -x -e
+set -e
 
 credhub l 
 
@@ -11,10 +11,19 @@ wget https://s3.amazonaws.com/credhub-cli-tarballs/credhub-linux-1.5.0-beta.46.t
 tar xvf credhub-linux-1.5.0-beta.46.tgz
 
 echo $CAS_TO_ROTATE | jq -r .[] | while read object; do
+  #Existing Value
+  OLDCAID="$(./credhub g -n $object --output-json | jq -r .id)"
+
+  #Generate New Value and Assign ID
   ./credhub regenerate --name $object > /dev/null
+  NEWCAID="$(./credhub g -n $object --output-json | jq -r .id)"
+
   #Hackily set the value to include the prior certificate for gradual rotation
-  OLDCA="$(./credhub g -n $object --versions 2 --output-json | jq -r .versions[1].value.certificate)"
-  NEWCA="$(./credhub g -n $object --versions 2 --output-json | jq -r .versions[0].value.certificate)"
-  ./credhub set --type certificate --name $object --certificate "${NEWCA}\n${OLDCA}" --private "$(./credhub g -n $object --output-json | jq -r .value.private_key)"
+  OLDCA="$(./credhub g --id $OLDCAID --output-json | jq -r .value.certificate)"
+  NEWCA="$(./credhub g --id $NEWCAID --output-json | jq -r .value.certificate)"
+  ./credhub set --type certificate --name $object --certificate "${NEWCA}\n${OLDCA}" --private "$(./credhub g --id $NEWCAID --output-json | jq -r .value.private_key)"
+  
+  echo "Rotating ${object}. Existing value: ${OLDCAID}. New Value: ${NEWCAID}\n"
+ 
 done
 
--- tasks/rotate-cas-part3.sh
+++ tasks/rotate-cas-part3.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-set -x -e
+set -e
 
 credhub l 
 
@@ -11,7 +11,12 @@ wget https://s3.amazonaws.com/credhub-cli-tarballs/credhub-linux-1.5.0-beta.46.t
 tar xvf credhub-linux-1.5.0-beta.46.tgz
 
 echo $CAS_TO_ROTATE | jq -r .[] | while read object; do
+  #Value of New CA is the previous value, before the stacked version
+  NEWCAID="$(./credhub g -n $object --versions 2 --output-json | jq -r .versions[1].id)" 
+
   #Hackily unset the value to remove the prior certificate
-  ./credhub set --type certificate --name $object --certificate "$(./credhub g -n $object --versions 2 --output-json | jq -r .versions[1].value.certificate)" --private "$(./credhub g -n $object --versions 2 --output-json | jq -r .versions[1].value.private_key)" 
+  ./credhub set --type certificate --name $object --certificate "$(./credhub g --id $NEWCAID --output-json | jq -r .value.certificate)" --private "$(./credhub g --id $NEWCAID --output-json | jq -r .value.private_key)" 
+
+  echo "Setting CA to new value: ${NEWCAID}\n"
 done
 
