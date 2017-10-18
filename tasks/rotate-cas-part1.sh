3264073c904968e48bde3434e6494715439c6f18
Dan Jahner <djahner@pivotal.io>
2017-10-18 13:31:16 -0700

    Ensure order of CAs in stack

--- tasks/rotate-cas-part1.sh
+++ tasks/rotate-cas-part1.sh
@@ -13,6 +13,8 @@ tar xvf credhub-linux-1.5.0-beta.46.tgz
 echo $CAS_TO_ROTATE | jq -r .[] | while read object; do
   ./credhub regenerate --name $object > /dev/null
   #Hackily set the value to include the prior certificate for gradual rotation
-  ./credhub set --type certificate --name $object --certificate "$(./credhub g -n $object --versions 2 --output-json | jq -r .versions[1].value.certificate & ./credhub g -n $object --versions 2 --output-json | jq -r .versions[0].value.certificate)" --private "$(./credhub g -n $object --output-json | jq -r .value.private_key)"
+  OLDCA="$(./credhub g -n $object --versions 2 --output-json | jq -r .versions[1].value.certificate)"
+  NEWCA="$(./credhub g -n $object --versions 2 --output-json | jq -r .versions[0].value.certificate)"
+  ./credhub set --type certificate --name $object --certificate "${NEWCA}\n${OLDCA}" --private "$(./credhub g -n $object --output-json | jq -r .value.private_key)"
 done
 
