c07becb7729d1e84b416ff83e87d82ba26fca7af
Dan Jahner <djahner@pivotal.io>
2017-10-19 13:30:59 -0700

    Request needs to handle multiline private key

--- tasks/rotate-jwt-keys-part1.sh
+++ tasks/rotate-jwt-keys-part1.sh
@@ -5,13 +5,20 @@ set -e
 credhub l 
 
 echo $KEYS_TO_ROTATE | jq -r .[] | while read object; do
-  OLDKEY="$(credhub g -n $object --output-json | jq -r .value.private_key)"
+  OLDKEY="$(credhub g -n $object --output-json | jq .value.private_key)"
   OLDKEYID="$(credhub g -n $object --output-json | jq -r .id)"
 
-  NEWKEY="$(credhub r -n $object --output-json | jq -r .value.private_key)"
+  NEWKEY="$(credhub r -n $object --output-json | jq .value.private_key)"
   NEWKEYID="$(credhub g -n $object --output-json | jq -r .id)"
+ 
+  JSON="$(cat <<EOF
+  {
+    "active-key": {"signingKey":${NEWKEY}},
+    "inactive-key": {"signingKey":${OLDKEY}}
+  }
+  EOF)"
 
-  credhub s -t json -n "${object}_object" -v "{\"active-key\":{\"signingKey\":\"${NEWKEY}\"},\"inactive-key\":{\"signingKey\":\"${OLDKEY}\"}}" > /dev/null
+  credhub s -t json -n "${object}_object" -v "${JSON}" > /dev/null
 
   printf "Rotating ${object}\n  Existing Value: ${OLDKEYID}\n  New Value: ${NEWKEYID}\n\n"
 
