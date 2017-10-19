77ffbee1e91930b17bcbc7370cadc9d8b04b2417
Dan Jahner <djahner@pivotal.io>
2017-10-19 10:49:28 -0700

    Add consul encryption key jobs

---
+++ tasks/rotate-consul-keys-part1.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+set -e
+
+credhub l 
+
+echo $KEYS_TO_ROTATE | jq -r .[] | while read object; do
+  OLDKEY="$(credhub g -n $object --output-json | jq -r .value)"
+  OLDKEYID="$(credhub g -n $object --output-json | jq -r .id)"
+
+  NEWKEY="$(credhub r -n $object --output-json | jq -r .value)"
+  NEWKEYID="$(credhub g -n $object --output-json | jq -r .id)"
+
+  credhub s -t json -n "${object}-list" -v "{\"list\":[\"${NEWKEY}\",\"${OLDKEY}\"]}" > /dev/null
+
+  printf "Rotating ${object}\n  Existing Value: ${OLDKEYID}\n  New Value: ${NEWKEYID}\n\n"
+
+done
+
---
+++ tasks/rotate-consul-keys-part2.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+set -e
+
+credhub l 
+
+echo $KEYS_TO_ROTATE | jq -r .[] | while read object; do
+  OLDKEYID="$(credhub g -n $object --versions 2 --output-json | jq -r .versions[1].id)"
+
+  NEWKEY="$(credhub g -n $object --output-json | jq -r .value)"
+  NEWKEYID="$(credhub g -n $object --output-json | jq -r .id)"
+
+  credhub s -t json -n "${object}-list" -v "{\"list\":[\"${NEWKEY}\"]}" > /dev/null
+
+  printf "Rotating ${object}\n   New Value: ${NEWKEYID}\n   Removed Value: ${OLDKEYID}\n\n"
+
+done
+
