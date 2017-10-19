22dac464429d6338fec1dca7dd764ae04e387cbf
Dan Jahner <djahner@pivotal.io>
2017-10-19 14:31:52 -0700

    BBS rotation needs to change active key names

--- tasks/rotate-bbs-keys-part1.sh
+++ tasks/rotate-bbs-keys-part1.sh
@@ -5,13 +5,32 @@ set -e
 credhub l 
 
 echo $KEYS_TO_ROTATE | jq -r .[] | while read object; do
+  #BBS stores the label of the encryption key, so we cannot just move the old key from label 'active-key' to 'inactive-key'
+  #We must set the active key name in a variable and track it in the rotation
+  OLDKEYNAME="$(credhub g -n ${object}_name --output-json | jq -r .value)"
   OLDKEY="$(credhub g -n $object --output-json | jq -r .value)"
   OLDKEYID="$(credhub g -n $object --output-json | jq -r .id)"
 
+  NEWKEYNAME="$(credhub r -n ${object}_name --output-json | jq -r .value)"
   NEWKEY="$(credhub r -n $object --output-json | jq -r .value)"
   NEWKEYID="$(credhub g -n $object --output-json | jq -r .id)"
 
-  credhub s -t json -n "${object}_object" -v "{\"list\":[{\"label\":\"active-key\",\"passphrase\":\"${NEWKEY}\"},{\"label\":\"inactive-key\",\"passphrase\":\"${OLDKEY}\"}]}" > /dev/null
+  JSON="$(cat <<EOF
+{
+  "list": [
+    {
+      "label": "${OLDKEYNAME}",
+      "passphrase": "${OLDKEY}"
+    },
+    {
+      "label": "${NEWKEYNAME}",
+      "passphrase": "${NEWKEY}"
+    }]
+}
+EOF
+)"
+
+  credhub s -t json -n "${object}_object" -v "${JSON}" > /dev/null
 
   printf "Rotating ${object}\n  Existing Value: ${OLDKEYID}\n  New Value: ${NEWKEYID}\n\n"
 
--- tasks/rotate-bbs-keys-part2.sh
+++ tasks/rotate-bbs-keys-part2.sh
@@ -10,10 +10,11 @@ tar xvf credhub-linux-1.5.0-beta.46.tgz
 echo $KEYS_TO_ROTATE | jq -r .[] | while read object; do
   OLDKEYID="$(./credhub g -n $object --versions 2 --output-json | jq -r .versions[1].id)"
 
+  NEWKEYNAME="$(credhub g -n ${object}_name --output-json | jq -r .value)"
   NEWKEY="$(./credhub g -n $object --output-json | jq -r .value)"
   NEWKEYID="$(./credhub g -n $object --output-json | jq -r .id)"
 
-  ./credhub s -t json -n "${object}_object" -v "{\"list\":[{\"label\":\"active-key\",\"passphrase\":\"${NEWKEY}\"}]}" > /dev/null
+  ./credhub s -t json -n "${object}_object" -v "{\"list\":[{\"label\":\"${NEWKEYNAME}\",\"passphrase\":\"${NEWKEY}\"}]}" > /dev/null
 
   printf "Rotating ${object}\n  Removed Value: ${OLDKEYID}\n  New Value: ${NEWKEYID}\n\n"
 
