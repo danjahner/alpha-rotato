e3febaa7a10fbbc85a6d2e086b8d9e6b987cf5a1
Dan Jahner <djahner@pivotal.io>
2017-10-19 17:29:22 -0700

    Make jwt part 2 newline friendly

--- tasks/rotate-jwt-keys-part2.sh
+++ tasks/rotate-jwt-keys-part2.sh
@@ -10,10 +10,17 @@ tar xvf credhub-linux-1.5.0-beta.46.tgz
 echo $KEYS_TO_ROTATE | jq -r .[] | while read object; do
   OLDKEYID="$(./credhub g -n $object --versions 2 --output-json | jq -r .versions[1].id)"
 
-  NEWKEY="$(./credhub g -n $object --output-json | jq -r .value.private_key)"
+  NEWKEY="$(./credhub g -n $object --output-json | jq .value.private_key)"
   NEWKEYID="$(./credhub g -n $object --output-json | jq -r .id)"
 
-  ./credhub s -t json -n "${object}_object" -v "{\"active-key\":{\"signingKey\":\"${NEWKEY}\"}}" > /dev/null
+  JSON="$(cat <<EOF
+{
+  "active-key": {"signingKey":${NEWKEY}}
+}
+EOF
+)"
+
+  ./credhub s -t json -n "${object}_object" -v "$JSON" > /dev/null
 
   printf "Rotating ${object}\n  Removed Value: ${OLDKEYID}\n  New Value: ${NEWKEYID}\n\n"
 
