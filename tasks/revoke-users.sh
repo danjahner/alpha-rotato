f2d19bcf5570cf4a4988ad93e461228502be6f42
Dan Jahner <djahner@pivotal.io>
2017-10-26 10:27:34 -0400

    Update revoke users script to use BOSH SSH

--- tasks/revoke-users.sh
+++ tasks/revoke-users.sh
@@ -2,9 +2,13 @@
 
 set -e
 
+echo "$BOSH_JUMPBOX_SSH_KEY" > jump.pem
+
 echo $USERS_TO_REVOKE_PRIOR | jq -r .[] | while read object; do
+  //MySQL is not exposed publically, so we need to use BOSH SSH to send the mysql command
   user="$(credhub g -n $object --versions 2 --output-json | jq -r .versions[1].value.username)"
-  mysql -h ${MYSQL_HOST} -u ${MYSQL_USERNAME} -p ${MYSQL_PASSWORD} -e "DROP USER IF EXISTS ${user};"
+
+  bosh ssh mysql/0 --gw-private-key jump.pem -c "mysql -h${MYSQL_HOST} -u${MYSQL_USERNAME} -p${MYSQL_PASSWORD} -e\"DROP USER IF EXISTS ${user}\"";
 
   printf "Revoked access for user ${user}\n"
 done
