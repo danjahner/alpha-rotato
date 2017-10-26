4165750e6bb31d486cb58fa009ae3c9cf9e78b74
Dan Jahner <djahner@pivotal.io>
2017-10-26 10:55:32 -0400

    MySQL admin password is in another namespace, so we need to do something bad to get it

--- tasks/revoke-users.sh
+++ tasks/revoke-users.sh
@@ -5,11 +5,14 @@ set -e
 echo "$BOSH_JUMPBOX_SSH_KEY" > jump.pem
 chmod 400 jump.pem
 
+credhub l
+mysqladmin="$(credhub g -n ${MYSQL_PASSWORD_REF} --output-json | jq -r .value)"
+
 echo $USERS_TO_REVOKE_PRIOR | jq -r .[] | while read object; do
   # MySQL is not exposed publically, so we need to use BOSH SSH to send the mysql command
   user="$(credhub g -n $object --versions 2 --output-json | jq -r .versions[1].value.username)"
 
-  bosh ssh mysql/0 --gw-private-key jump.pem -c "mysql -h${MYSQL_HOST} -u${MYSQL_USERNAME} -p${MYSQL_PASSWORD} -e\"DROP USER IF EXISTS ${user}\"";
+  bosh ssh mysql/0 --gw-private-key jump.pem -c "mysql -u${MYSQL_USERNAME} -p${mysqladmin} -e\"DROP USER IF EXISTS ${user}\"";
 
   printf "Revoked access for user ${user}\n"
 done
